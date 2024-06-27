82c2cb033243a59b96e23dc10aa30979976379e9
Nefta Hernandez <neftaligow01@gmail.com>
2024-06-26 22:56:40 -0600

    chore: Update backend configuration for CORS and logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import cors from "cors";
+
 dotenv.config();
 
-import userRoute from "./routes/user.routes"
+import userRoute from "./routes/user.routes";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+app.use(morgan("dev"));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use(cookieParser());
 app.use(express.json());
