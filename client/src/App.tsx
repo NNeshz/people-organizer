a1753eaa93f0602c9a8d37e04ea9bb2ddea6f262
Nefta Hernandez <neftaligow01@gmail.com>
2024-06-26 21:08:36 -0600

    chore: Configure TypeScript paths and update dependencies, installed Shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-red-500">React App</h1>
+      <Button>React App</Button>
     </>
   )
 }
